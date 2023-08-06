70a53536adcb4388abe6d9c29115370b6512a80f
James Wu <jameswu144@gmail.com>
2023-08-06 21:45:17 +0100

    create card struct
    6 Aug 23
    
    Implemented the card struct with functions that can get its suit and rank, its value and an override
    ToString function for displaying.

---
+++ Card.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blackjack {
+    public struct Card {
+        public int id;
+
+        public Card(int id) {
+            this.id = id;
+        }
+
+        public int GetValueBlackJack() {
+            string rank = GetRank();
+            return rank switch {
+                "A" => 1,
+                "T" => 10,
+                "J" => 10,
+                "Q" => 10,
+                "K" => 10,
+                _ => int.Parse(rank)
+            };
+        }
+
+        public string GetRank() {
+            int mask = 0b00001111;
+            int value = id & mask;
+            return value switch {
+                1 => "A",
+                10 => "T",
+                11 => "J",
+                12 => "Q",
+                13 => "K",
+                _ => value.ToString()
+            };
+        }
+        
+        public string GetSuit() {
+            Dictionary<int, string> map = new() {
+                {0b10000000, "♠"},
+                {0b01000000, "♥"},
+                {0b00100000, "♥"},
+                {0b00010000, "♦"},
+            };
+
+            int mask = 0b11110000;
+            int suit = id & mask;
+            return map[suit];
+        }
+
+        public override string ToString()
+        {
+            return $"{GetRank()}{GetSuit()}";
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blackjack {
+    public class Program {
+        public static void Main() {
+            Card card = new Card(0b01001010);
+            Console.WriteLine(card);
+            Console.WriteLine(card.GetValueBlackJack());
+        }
+    }
+}
